4e727215914beb8471220d3b06321d6d6e6babc6
Carlo Lepelaars <info@carlolepelaars.nl>
2020-04-22 21:13:35 +0200

    Ghost bottleneck

--- ghost_bottleneck/bottleneck.py
+++ ghost_bottleneck/bottleneck.py
@@ -1,14 +1,14 @@
-from tensorflow.keras.layers import GlobalAveragePooling2D, Conv2D, DepthwiseConv2D, Layer, BatchNormalization, Activation
+from tensorflow.keras.layers import Conv2D, DepthwiseConv2D, Layer, BatchNormalization, Activation, add
 
 from ghost_bottleneck.components.semodule import SEModule
 from ghost_bottleneck.components.ghostmodule import GhostModule
 
 
-# TODO Finish bottleneck
 class GBNeck(Layer):
     def __init__(self, dwkernel, strides, exp, out, ratio, use_se):
         super(Layer, self).__init__()
         self.strides = strides
+        self.use_se = use_se
         self.batchnorm = BatchNormalization()
         self.conv = Conv2D(out, (1,1), strides=(1,1), padding='same', data_format='channels_last',
                            activation=None, use_bias=False)
@@ -17,6 +17,7 @@ class GBNeck(Layer):
                                          data_format='channels_last', activation=None, use_bias=False)
         self.ghost1 = GhostModule(exp, ratio, 1, 3, use_bias=False)
         self.ghost2 = GhostModule(out, ratio, 1, 3, use_bias=False)
+        self.se = SEModule(exp, ratio)
 
     def call(self, inputs):
         x = self.depthconv(inputs)
@@ -27,35 +28,15 @@ class GBNeck(Layer):
         y = self.ghost1(x)
         y = self.batchnorm(y)
         y = self.relu(y)
-
+        # Extra depth conv if strides higher than 1
         if self.strides > 1:
             y = self.depthconv(y)
             y = self.batchnorm(y)
-
-
-
-
-        return x
-
-
-def GhostBottleneck(x,dwkernel,strides,exp,out,ratio,use_se):
-    x1 = DepthwiseConv2D(dwkernel,strides,padding='same',depth_multiplier=ratio-1,data_format='channels_last',
-                         activation=None,use_bias=False)(x)
-    x1 = BatchNormalization(axis=-1)(x1)
-    x1 = Conv2D(out,(1,1),strides=(1,1),padding='same',data_format='channels_last',
-               activation=None,use_bias=False)(x1)
-    x1 = BatchNormalization(axis=-1)(x1)
-    y = GhostModule(x,exp,ratio,1,3)
-    y = BatchNormalization(axis=-1)(y)
-    y = Activation('relu')(y)
-    if(strides>1):
-        y = DepthwiseConv2D(dwkernel,strides,padding='same',depth_multiplier=ratio-1,data_format='channels_last',
-                         activation=None,use_bias=False)(y)
-        y = BatchNormalization(axis=-1)(y)
-        y = Activation('relu')(y)
-    if(use_se==True):
-        y = SEModule(y,exp,ratio)
-    y = GhostModule(y,out,ratio,1,3)
-    y = BatchNormalization(axis=-1)(y)
-    y = add([x1,y])
-    return y
+            y = self.relu(y)
+        # Squeeze and excite
+        if self.use_se:
+            y = self.se(y)
+        y = self.ghost2(y)
+        y = self.batchnorm(y)
+        # Skip connection
+        return add([x, y])
