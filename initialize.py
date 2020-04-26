ac8b04fc9c9aa9dcbf47c6f25eec4c2454d20be9
Carlo Lepelaars <info@carlolepelaars.nl>
2020-04-26 13:49:10 +0200

    Move initialize

--- initialize.py
+++ initialize.py
@@ -1,4 +1,3 @@
-import tensorflow as tf
 from ghost_model import GhostNet
 
 if __name__ == "__main__":
@@ -7,9 +6,4 @@ if __name__ == "__main__":
 
     # Build model with specified shape
     model.build((None, 224, 224, 3))
-
-    # Compile and fit
-    # model.compile(loss='categorical_crossentropy',
-    #               optimizer='adam', metrics=['accuracy'])
-    # model.fit(tf.ones((8, 224, 224, 3)))
     print(model.summary())
